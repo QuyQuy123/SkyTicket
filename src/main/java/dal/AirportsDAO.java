9c1f373bd9404ad2b77f8e3bebce5a43e6c661cb
Hieund <Hieundhe187180@fpt.edu.vn>
2025-02-10 10:00:20 +0700

    add updateAirport method

--- src/main/java/dal/AirportsDAO.java
+++ src/main/java/dal/AirportsDAO.java
@@ -2,8 +2,11 @@ package dal;
 
 import model.Airports;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AirportsDAO extends DBConnect {
 
@@ -24,6 +27,46 @@ public class AirportsDAO extends DBConnect {
             throw new RuntimeException(e);
         }
         return n;
+    }
+
+    public int updateAirport(Airports ap) {
+        int n = 0;
+        String sql = "UPDATE `skytickets`.`airports`\n" +
+                "SET\n" +
+                "`AirportId` = ?,\n" +
+                "`AirportName` = ?,\n" +
+                "`LocationId` = ?,\n" +
+                "`Status` = ?\n" +
+                "WHERE `AirportId` = ?;";
+        try {
+            PreparedStatement pre = connection.prepareStatement(sql);
+            pre.setInt(1, ap.getAirportId());
+            pre.setString(2, ap.getAirportName());
+            pre.setInt(3, ap.getLocationId());
+            pre.setInt(4, ap.getStatus());
+            n = pre.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return n;
+    }
+
+    public List<Airports> getAllAirports(String sql){
+        List<Airports> list = new ArrayList<>();
+        try(PreparedStatement ps = connection.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            while(rs.next()){
+                Airports airports = new Airports();
+                airports.setAirportId(rs.getInt("AirportId"));
+                airports.setAirportName(rs.getString("AirportName"));
+                airports.setLocationId(rs.getInt("LocationId"));
+                airports.setStatus(rs.getInt("Status"));
+                list.add(airports);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public static void main(String[] args) {
